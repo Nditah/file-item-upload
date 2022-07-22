31b275654de0b6ab3307a0a8ba76a601500a6907
Nditah <nditah@gmail.com>
2022-07-22 23:24:58 +0100

    ok

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
-const express = require('express');
+const express = require("express");
 const fileupload = require("express-fileupload");
-require('dotenv').config()
+require("dotenv").config();
 
 const {
-  s3Client,
   list,
-  upload,
   listBuckets,
   createBucket,
   uploadToBucket,
   listFilesInBucket,
   deleteBucket,
-  binaryHandler,
-  base64Handler,
   uploadService,
-} = require('./s3-service');
+} = require("./s3-service");
 
 const app = express();
 
-
 // Views in public directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Main, error and success views
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.get("/success", function (req, res) {
-  res.sendFile(__dirname + '/public/success.html');
+  res.sendFile(__dirname + "/public/success.html");
 });
 
 app.get("/error", function (req, res) {
-  res.sendFile(__dirname + '/public/error.html');
+  res.sendFile(__dirname + "/public/error.html");
 });
 
-app.post('/file-upload', fileupload(),  function (req, res, next) {
-  console.log(req.files);
-  // upload(req, res, function (error) {
-  //   if (error) {
-  //     console.log(error);
-  //     return res.redirect("/error");
-  //   }
-  //   console.log('File uploaded successfully.');
-  //   res.redirect("/success");
-  // });
-  res.redirect("/success");
+app.post("/file-upload", fileupload(), function (req, res, next) {
+  try {
+    const data = {
+      type: 'PHOTO',
+      ...req.body,
+      file: req?.files?.file1,
+    };
+    console.log(req.files)
+    uploadService(data)
+    .then(result => console.log(result))
+    .catch((err) => {
+      throw new Error(err.message);
+    });
+    console.log("File uploaded successfully.");
+    res.redirect("/success");
+  } catch (error) {
+    console.log(error);
+    return res.redirect("/error");
+  }
 });
 
 app.listen(3001, function () {
-  console.log('Server listening on port 3001.');
+  console.log("Server listening on port 3001.");
   // list().then()
 });
--- s3-service.js
+++ s3-service.js
@@ -1,11 +1,13 @@
-const { S3, Endpoint } = require('aws-sdk');
-const { PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3, Endpoint } = require("aws-sdk");
 const { extname } = require("path");
 
-// https://gifti.fra1.digitaloceanspaces.com 
+// https://gifti.fra1.digitaloceanspaces.com
+const AWS_BUCKET = "gifti";
+
+// const url = `https://${folder}.s3.${REGION}.amazonaws.com/${fileName}`;
 
 // Set S3 endpoint to DigitalOcean Spaces
-const spacesEndpoint = new Endpoint('fra1.digitaloceanspaces.com');
+const spacesEndpoint = new Endpoint("fra1.digitaloceanspaces.com");
 
 const s3Client = new S3({
   endpoint: spacesEndpoint,
@@ -20,223 +22,144 @@ const s3Client = new S3({
  * Call S3 to list the buckets
  */
 
-function listBuckets(){
-    s3Client.listBuckets(function(err, data) {
-        if (err) {
-        console.log("Error", err);
-        } else {
-        console.log("Success", data.Buckets);
-        }
-    });
-  }
-  
-  /**
-  * call S3 to create the bucket
-  */
-  
-function createBucket(name){
-     const bucketParams = {
-        Bucket: name,
-      };
-    s3Client.createBucket(bucketParams, function(err, data) {
+function listBuckets() {
+  s3Client.listBuckets(function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data.Buckets);
+    }
+  });
+}
+
+/**
+ * call S3 to create the bucket
+ */
+
+function createBucket(name) {
+  const bucketParams = {
+    Bucket: name,
+  };
+  s3Client.createBucket(bucketParams, function (err, data) {
     if (err) {
       console.log("Error", err);
     } else {
       console.log("Success", data.Location);
     }
   });
-  }
-  
-  /**
-  * Uploading a File to an Amazon S3 Bucket
-  */
-  
-function uploadToBucket(bucketName, fileName){
-    const uploadParams = {Bucket: bucketName, Key: '', Body: ''};
-    const file = fileName;
-    
-    // Configure the file stream and obtain the upload parameters
-    const fileStream = fs.createReadStream(file);
-    fileStream.on('error', function(err) {
-      console.log('File Error', err);
-    });
-    uploadParams.Body = fileStream;
-    uploadParams.Key = path.basename(file);
-    
-    // call S3 to retrieve upload file to specified bucket
-    s3Client.upload (uploadParams, function (err, data) {
-      if (err) {
-        console.log("Error", err);
-      } if (data) {
-        console.log("Upload Success", data.Location);
-      }
-    });
-  }
-  
-  
-  
-  /**
-  * Listing Objects in an Amazon S3 Bucket
-  */
-  
-function listFilesInBucket(bucketName){
-    const bucketParams = {
-        Bucket : bucketName,
-      };
-      // Call S3 to obtain a list of the objects in the bucket
-      s3Client.listObjects(bucketParams, function(err, data) {
-        if (err) {
-          console.log("Error", err);
-        } else {
-          console.log("Success", data);
-        }
-      });
-  }
-  
-  
-  /**
-  * Deleting an Amazon S3 Bucket
-  */
-  
-function deleteBucket(bucketName){
-    const bucketParams = {
-        Bucket : bucketName,
-      };
-    // Call S3 to delete the bucket
-    s3Client.deleteBucket(bucketParams, function(err, data) {
-        if (err) {
-        console.log("Error", err);
-        } else {
-        console.log("Success", data);
-        }
-    });
-  }
-  
+}
 
+/**
+ * Uploading a File to an Amazon S3 Bucket
+ */
 
-async function binaryHandler(req, res) {
-    try {
-      const { folder, createdBy } = req.body;
-      const name = slugit(req.body.name);
-        const file = req.files.image;
-        const ext = extname(file.name);
-        const fileName = `${folder}/${new Date()
-          .getTime()
-          .toString()}-${createdBy}.${ext}`;
-        const binaryData = new Buffer.from(file.data, "binary");
-        const fileContent = {
-          Bucket: folder,
-          Key: fileName, //* Need to maintain unique images 
-          Body: binaryData,
-          ACL: 'public-read',
-          ContentEncoding: 'base64', // required
-          ContentType: `image/${ext}` // required. Notice the back ticks
-        };
-        const url = `https://${folder}.s3.${REGION}.amazonaws.com/${fileName}`;
-        delete req.body.binaryString;
-        const record = { ...req.body, url, fileContent };
-        const result = await uploadService(record);
-        return success(res, 201, record);
-      } catch (error) {
-        loging(module, req, error);
-        return fail(res, 400, `${error.message}`);
-      }
+function uploadToBucket(bucketName, fileName) {
+  const uploadParams = { Bucket: bucketName, Key: "", Body: "" };
+  const file = fileName;
+
+  // Configure the file stream and obtain the upload parameters
+  const fileStream = fs.createReadStream(file);
+  fileStream.on("error", function (err) {
+    console.log("File Error", err);
+  });
+  uploadParams.Body = fileStream;
+  uploadParams.Key = path.basename(file);
+
+  // call S3 to retrieve upload file to specified bucket
+  s3Client.upload(uploadParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
     }
-  
-  async function base64Handler(req, res) {
-      try {
-        const { base64String, folder, createdBy } = req.body;
-        const name = slugit(req.body.name); // -${new Date().getTime().toString()}
-        const ext = base64String.split(';')[0].split('/')[1]; // Extension
-        const fileName = `${folder}/${name}-${createdBy}.${ext}`;
-        const base64Data = new Buffer.from(base64String.replace(/^data:image\/\w+;base64,/, ""), 'base64');
-        const fileContent = {
-          Bucket: folder,
-          Key: fileName, //* Need to maintain unique images 
-          Body: base64Data,
-          ACL: 'public-read',
-          ContentEncoding: 'base64', // required
-          ContentType: `image/${ext}` // required. Notice the back ticks
-        };
-        const url = `https://${folder}.s3.${REGION}.amazonaws.com/${fileName}`;
-        delete req.body.base64String;
-        const record = { ...req.body, url, fileContent };
-        const result = await uploadService(record);
-        return success(res, 201, result);
-      } catch (error) {
-        loging(module, req, error);
-        return fail(res, 400, `${error.message}`);
-      }
+    if (data) {
+      console.log("Upload Success", data.Location);
     }
-  
-  async function uploadService(data) {
-      try {
-        const { error } = validateCreate.validate(data);
-        if (error) throw new Error(`Error validating ${module} data. ${error.message}`);
-        const command = new PutObjectCommand(data.fileContent);
-        const response = await s3Client.send(command);
-        if (response && response['$metadata'] && response['$metadata'].httpStatusCode === 200) {
-          const newRecord = new Media(data);
-          const result = await newRecord.save();
-          return result;
-        }
-        throw new Error("unable to upload file");
-      } catch (err) {
-        throw new Error(`Error creating file record. ${err.message}`);
-      }
+  });
+}
+
+/**
+ * Listing Objects in an Amazon S3 Bucket
+ */
+
+function listFilesInBucket(bucketName) {
+  const bucketParams = {
+    Bucket: bucketName,
+  };
+  // Call S3 to obtain a list of the objects in the bucket
+  s3Client.listObjects(bucketParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
     }
-   
+  });
+}
+
+/**
+ * Deleting an Amazon S3 Bucket
+ */
+
+function deleteBucket(bucketName) {
+  const bucketParams = {
+    Bucket: bucketName,
+  };
+  // Call S3 to delete the bucket
+  s3Client.deleteBucket(bucketParams, function (err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      console.log("Success", data);
+    }
+  });
+}
 
 async function list() {
-    const params = {
-      Bucket: 'gifti',
-      // Delimiter: '/PHOTO',
-    };
-    const result = await s3Client.listObjects(params).promise();
-    console.dir(result);
-    return result;
-  }
-  
-  
-  async function upload(data) {
-    const time = new Date().toISOString().slice(0, 22);
-    const fileData = {
-      Bucket: this.amazonConfig.awsBucket,
-      Key: `${data.type}/photo-${time}`,
-      ACL: 'public-read',
-    };
-    if (data.base64String) {
-      fileData.ContentEncoding = 'base64'; // required
-      fileData.Body = Buffer.from(
-        data.base64String.replace(/^data:image\/\w+;base64,/, ''),
-        'base64'
-      );
-      const ext = data.base64String.split(';')[0].split('/')[1]; // Extension
-      fileData.Key = `${fileData.Key}.${ext}`;
-      fileData.ContentType = `image/${ext}`;
-    } else if (data.dataBuffer) {
-      fileData.Body = data.dataBuffer;
-    } else {
-      throw new Error(
-        'Invalid request data: base64String and dataBuffer can not be null'
-      );
-    }
-    // console.log({ fileData });
-    const uploadResult = await s3Client.upload(fileData).promise();
-    // console.log({ uploadResult });
-    return uploadResult;
-  }
-  
-  module.exports = {
-    s3Client,
-    list,
-    upload,
-    listBuckets,
-    createBucket,
-    uploadToBucket,
-    listFilesInBucket,
-    deleteBucket,
-    binaryHandler,
-    base64Handler,
-    uploadService,
+  const params = {
+    Bucket: AWS_BUCKET,
+    // Delimiter: '/PHOTO',
   };
+  const result = await s3Client.listObjects(params).promise();
+  console.dir(result);
+  return result;
+}
+
+async function uploadService(data) {
+  const { type, file, base64String, folder } = data;
+  const time = `${new Date().getTime().toString()}`;
+  const fileData = {
+    Bucket: AWS_BUCKET,
+    Key: `${type}/${folder}/${time}`,
+    ACL: "public-read",
+  };
+  let ext = '';
+  if (base64String) {
+    fileData.ContentEncoding = "base64"; // required
+    fileData.Body = Buffer.from(base64String.replace(/^data:image\/\w+;base64,/, ""),
+      "base64"
+    );
+    ext = base64String.split(";")[0].split("/")[1]; // Extension
+    fileData.ContentType = `image/${ext}`;
+    fileData.Key = `${fileData.Key}.${ext}`;
+  } else if (file) {
+    ext = extname(file.name);
+    fileData.Key = `${fileData.Key}${ext}`;
+    fileData.ContentType = file.mimetype;
+    fileData.Body = file.data;
+  } else {
+    throw new Error(
+      "Invalid request data: base64String and dataBuffer can not be null"
+    );
+  }
+  console.log(fileData);
+  const result = await s3Client.upload(fileData).promise();
+  return result;
+}
+
+module.exports = {
+  list,
+  listBuckets,
+  createBucket,
+  uploadToBucket,
+  listFilesInBucket,
+  deleteBucket,
+  uploadService,
+};
